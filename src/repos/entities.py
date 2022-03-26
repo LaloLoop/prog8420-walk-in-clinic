fc54612f796951769847e95561bfb6ddd7559051
Eduardo Gutiérrez Silva <eduardo@yalochat.com>
2022-03-26 18:32:57 -0400

    feat: model implementation

--- src/repos/entities.py
+++ src/repos/entities.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
+import datetime
 
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
@@ -22,13 +23,92 @@ class Person(Base):
 
     employee = relationship("Employee", back_populates="person")
 
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
 
 class Employee(Base):
     __tablename__ = 'Employee'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     person_id = Column(Integer, ForeignKey('Person.id'))
+    job_id = Column(Integer, ForeignKey('Job.id'))
     password = Column(String)
 
     person = relationship("Person")
+    job = relationship("Job", back_populates="employees")
 
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+
+class Job(Base):
+    __tablename__ = 'Job'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    speciality = Column(String)
+
+    employees = relationship("Employee", back_populates="job")
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+
+class Patient(Base):
+    __tablename__ = 'Patient'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    person_id = Column(Integer, ForeignKey('Person.id'))
+    ohip = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+    person = relationship("Person")
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+
+class Appointment(Base):
+    __tablename__ = 'Appointment'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    doctor_id = Column(Integer, ForeignKey('Employee.id'))
+    staff_id = Column(Integer, ForeignKey('Employee.id'))
+    patient_id = Column(Integer, ForeignKey('Patient.id'))
+    prescription_id = Column(Integer, ForeignKey('Prescription.id'))
+    date = Column(DateTime)
+    comments = Column(String)
+
+    staff = relationship("Employee", foreign_keys=[staff_id])
+    doctor = relationship("Employee", foreign_keys=[doctor_id])
+    patient = relationship("Patient")
+    prescription = relationship("Prescription")
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+
+class Unit(Base):
+    __tablename__ = 'Unit'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
+
+
+class Prescription(Base):
+    __tablename__ = 'Prescription'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    medication = Column(String)
+    quantity = Column(Integer)
+    unit_id = Column(Integer, ForeignKey('Unit.id'))
+
+    unit = relationship("Unit")
+
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
--- tests/test_entities.py
+++ tests/test_entities.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from src.repos.entities import Person, Base, Employee
+from src.repos.entities import Person, Base, Employee, Job, Patient, Appointment, Unit, Prescription
 
 person_data = {
     "first_name": "Marcos",
@@ -15,9 +17,56 @@ person_data = {
     "phone_number": "5195926738"
 }
 
+staff_data = {
+    "first_name": "Sarah",
+    "last_name": "Williams",
+    "birthdate": "1983-10-01",
+    "street": "101 Charles St",
+    "city": "Kitchener",
+    "province": "ON",
+    "postalcode": "N2HN2P",
+    "email": "sarah@conestogac.ca",
+    "phone_number": "5198920923"
+}
 
-def _create_test_person(engine, session):
-    person = Person(**person_data)
+patient_data = {
+    "first_name": "Elon",
+    "last_name": "Musk",
+    "birthdate": "1979-09-10",
+    "street": "12 El Paso",
+    "city": "Waterloo",
+    "province": "ON",
+    "postalcode": "N2HNT7",
+    "email": "elon@conestogac.ca",
+    "phone_number": "5196738983"
+}
+
+appointment_data = {
+    'date': datetime.fromisoformat('2022-04-22 10:10:10'),
+    'comments': 'Patient is healthy!'
+}
+
+job_dr_data = {
+    'title': 'Doctor',
+    'speciality': 'Neurologist'
+}
+
+job_staff_data = {
+    'title': 'Secretary',
+    'speciality': ''
+}
+
+advil_data = {
+    'medication': 'Advil',
+    'quantity': 12
+}
+
+
+def _create_test_person(session, data=None):
+    if data is None:
+        data = person_data
+
+    person = Person(**data)
 
     session.add(person)
     session.commit()
@@ -25,14 +74,85 @@ def _create_test_person(engine, session):
     return person
 
 
-def test_create_person():
+def _create_test_employee(session, person, job):
+    employee = Employee(password="super-secret")
+    employee.person = person
+    employee.job = job
+
+    session.add(employee)
+    session.commit()
+
+    return employee
+
+
+def _create_test_patient(session, person):
+    patient = Patient(ohip="3873933093")
+    patient.person = person
+
+    session.add(patient)
+    session.commit()
+
+    return patient
+
+
+def _create_test_job(session, job_data=None):
+    if job_data is None:
+        job_data = job_dr_data
+
+    job = Job(**job_data)
+
+    session.add(job)
+    session.commit()
+
+    return job
+
+
+def _create_test_unit(session, name):
+    unit = Unit(name=name)
+
+    session.add(unit)
+    session.commit()
+
+    return unit
+
+
+def _create_test_prescription(session, unit, prescription_data):
+    prescription = Prescription(**prescription_data)
+
+    prescription.unit = unit
+
+    session.add(prescription)
+    session.commit()
+
+    return prescription
+
+
+def _create_test_appointment(session, staff: Employee, patient: Patient, employee: Employee, prescription: Prescription,
+                             appointment_data: dict) -> Appointment:
+    appointment = Appointment(**appointment_data)
+    appointment.staff = staff
+    appointment.patient = patient
+    appointment.doctor = employee
+    appointment.prescription = prescription
+
+    session.add(appointment)
+    session.commit()
+
+    return appointment
+
+
+def _create_session():
     engine = create_engine('sqlite:///:memory:', echo=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    session = Session()
+    return Session()
 
-    person = _create_test_person(engine, session)
+
+def test_create_person():
+    session = _create_session()
+
+    _create_test_person(session)
 
     # Check the person was inserted
     result = session.query(Person).first()
@@ -42,23 +162,24 @@ def test_create_person():
         assert result.__dict__.get(k) == v
 
 
+def test_create_job():
+    session = _create_session()
+
+    _create_test_job(session)
+
+    result = session.query(Job).first()
+
+    assert result.id == 1
+    assert result.title == "Doctor"
+    assert result.speciality == "Neurologist"
+
+
 def test_create_employee():
-    engine = create_engine('sqlite:///:memory:', echo=True)
-    Base.metadata.create_all(engine)
+    session = _create_session()
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    person = _create_test_person(engine, session)
-    employee = Employee(password="super-secret")
-
-    employee.person = person
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    session.add(employee)
-    session.commit()
+    person = _create_test_person(session)
+    job = _create_test_job(session)
+    _create_test_employee(session, person, job)
 
     # Check the person was inserted
     result = session.query(Employee).first()
@@ -66,6 +187,79 @@ def test_create_employee():
     assert result.id == 1
     assert result.person_id == 1
     assert result.password == "super-secret"
+    assert result.job.id == job.id
+    assert result.job.title == job.title
+    assert result.job.speciality == job.speciality
 
-def test_create_job():
-    pass
+
+def test_create_patient():
+    session = _create_session()
+
+    person = _create_test_person(session)
+    _create_test_patient(session, person)
+
+    result = session.query(Patient).first()
+
+    assert person.id == 1
+    assert result.ohip == "3873933093"
+    assert result.person.id == person.id
+    assert result.person.first_name == person.first_name
+
+
+def test_create_prescription():
+    session = _create_session()
+
+    unit = _create_test_unit(session, "mg")
+
+    _create_test_prescription(session, unit, advil_data)
+
+    result = session.query(Prescription).first()
+
+    assert result.id == 1
+    assert result.medication == "Advil"
+    assert result.quantity == 12
+    assert result.unit.id == unit.id
+
+
+def test_create_appointment():
+    session = _create_session()
+
+    person = _create_test_person(session)
+    job = _create_test_job(session)
+    doctor = _create_test_employee(session, person, job)
+
+    unit = _create_test_unit(session, "mg")
+    prescription = _create_test_prescription(session, unit, advil_data)
+
+    staff = _create_test_employee(
+        session,
+        _create_test_person(session, staff_data),
+        _create_test_job(session, job_staff_data)
+    )
+
+    patient = _create_test_patient(
+        session,
+        _create_test_person(session, patient_data)
+    )
+
+    _create_test_appointment(session, staff, patient, doctor, prescription, appointment_data)
+
+    result = session.query(Appointment).first()
+
+    assert result.id == 1
+    assert result.patient.id == patient.id
+    assert result.doctor.id == doctor.id
+    assert result.staff.id == staff.id
+    assert result.date == appointment_data['date']
+    assert result.prescription.id == prescription.id
+
+
+def test_create_unit():
+    session = _create_session()
+
+    _create_test_unit(session, "mg")
+
+    result = session.query(Unit).first()
+
+    assert result.id == 1
+    assert result.name == "mg"
