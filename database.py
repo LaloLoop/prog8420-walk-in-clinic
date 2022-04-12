20df89f18f467c8bc85e0fa259185c02b08c62fe
Eduardo Gutiérrez Silva <gt.silva.e@gmail.com>
2022-04-12 03:33:17 -0400

    fix: format url for asyncpg

--- database.py
+++ database.py
@@ -5,7 +5,17 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./wic.sqlite")
+db_url = os.getenv("DATABASE_URL")
+
+PG_URL_PREFIX = 'postgres://'
+PG_ASYNC_URL_PREFIX = 'postgresql+asyncpg://'
+
+if db_url is not None and PG_URL_PREFIX in db_url:
+    db_url = db_url.replace(PG_URL_PREFIX, PG_ASYNC_URL_PREFIX)
+else:
+    "sqlite+aiosqlite:///./wic.sqlite"
+
+SQLALCHEMY_DATABASE_URL = db_url
 
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
